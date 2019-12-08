648b5de66442e0461e108f3b9902799c5247d87b
ichating <batis@foxmail.com>
2019-12-08 18:49:40 +0800

    init commit

---
+++ src/java_learn_code/no_16_object/Car.java
@@ -0,0 +1,15 @@
+package java_learn_code.no_16_object;
+
+public class Car {
+	String color;
+	int count ;
+	public void run(){
+		
+		int x=1;
+		/*for(int i = 0 ;  i < 4 ;i++){
+			
+		}*/
+		System.out.println("小汽车在跑 ..."+color+"..."+count);
+		System.out.println(x);
+	}
+}
---
+++ src/java_learn_code/no_16_object/Person.java
@@ -0,0 +1,5 @@
+package java_learn_code.no_16_object;
+
+public class Person {
+	String name ;
+}
