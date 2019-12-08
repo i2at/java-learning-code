09b6375566f58efd3f2a023795d07a843649eb1f
ichating <batis@foxmail.com>
2019-12-08 16:56:24 +0800

    OPO

---
+++ src/java_learn_code/no_15_java_for_collection/Car.java
@@ -0,0 +1,10 @@
+package java_learn_code.no_15_java_for_collection;
+
+public class Car {
+	String color;
+	int number;
+
+	void run() {
+		System.out.println(color + ":" + number);
+	}
+}
---
+++ src/java_learn_code/no_15_java_for_collection/CarDemo.java
@@ -0,0 +1,19 @@
+package java_learn_code.no_15_java_for_collection;
+
+public class CarDemo {
+
+	public static void main(String[] args) {
+		/*
+		 * 测试：Car类中的run方法。
+		 */
+		// 1,创建Car的对象。给对象起个名字。
+		Car c = new Car();// c是类类型的变量。c指向了一个具体的Car类型的对象。
+		// 2,通过已有的对象调用该对象的功能。格式：对象.对象成员;
+		// 3,可以该对象的属性赋值。
+		c.color = "red";
+		c.number = 4;
+		c.run();
+
+	}
+
+}
